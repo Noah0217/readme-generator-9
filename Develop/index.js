507140adb666c3b5a51f8ea30b99a0234d30acda
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-11 13:38:20 -0600

    started question array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util")
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input", 
+    message: "What is the Title for this project",
+    name: "Title"
+}, {
+    type: "input", 
+    message: "Give a rundown of how your project works",
+    name: "Description"
+}, {
+
+}, {
+    type: "input", 
+    message: "How would someone install your project",
+    name: "Installation"
+}, {
+
+}, {
+
+}
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
