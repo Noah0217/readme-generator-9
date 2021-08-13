5c728ac2434b8d96dbbd8988fba1498e4a0d24f3
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-13 15:06:27 -0600

    added installation and description to ReadME Markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,9 +12,16 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` 
+  # ${data.title}
+  https://github.com/${data.Username}/${data.title}
+  # Description
+  ${data.Description}
+  ${data.Installation}
+  
 
-`;
-}
+`
+};
+
 
 module.exports = generateMarkdown;
