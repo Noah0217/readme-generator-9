d1deab0d8e491fd8d761d060d40931ddaefe5cb4
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-14 23:26:33 -0600

    fixed name parameter error in node

--- Develop/index.js
+++ Develop/index.js
@@ -6,63 +6,65 @@ const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    type: "input", 
+    type: "input",
     message: "What is the Title for this project",
     name: "Title"
 }, {
-    type: "input", 
+    type: "input",
     message: "Give a brief overview of your project",
     name: "Description"
 }, {
-
+    type: "input",
+    message: "Table Of Contents",
+    name: "Table Of Contents"
 }, {
-    type: "input", 
-    message: "Whats tools need to be installed to run this project",
+    type: "input",
+    message: "What tools need to be installed to run this project",
     name: "Installation"
 }, {
-
+    type: "input",
+    message: "What Languages/Technologies were used to create this project",
+    name: "Languages/Technologies"
 }, {
-    type: "input", 
+    type: "input",
     message: "What License are you using for this project",
     name: "License"
 }, {
-    type: "input", 
+    type: "input",
     message: "Contact Info",
     name: "Questions"
 }, {
-    type: "input", 
+    type: "input",
     message: "What is your GitHub username",
     name: "Username"
 }, {
-    type: "input", 
+    type: "input",
     message: "What is your email address",
     name: "Email"
 },
-
 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, function(err) {
-            console.log(fileName)
-            console.log(data)
-            if (err) {
-                return console.log(err)
-            } else {
-                    console.log("Success")
-            }
+    fs.writeFile(fileName, data, function (err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("Success")
+        }
     })
 };
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(function(data) {
-        writeToFile("README.md", generateMarkdown(data));
-    })
+        .then(function (data) {
+            writeToFile("README.md", generateMarkdown(data));
+        })
 };
 
 // Function call to initialize app
 init();
-
